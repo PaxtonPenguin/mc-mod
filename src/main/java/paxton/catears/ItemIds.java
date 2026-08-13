7489b25e0d980a42a92d46a1c7b2a31a3e1f9ea9
Paxton Houston <Paxton.Houston11@gmail.com>
2026-08-13 13:46:55 -0500

    new commit so that the gecko discord server can see how awful this is /j

--- src/main/java/paxton/catears/ItemIds.java
+++ src/main/java/paxton/catears/ItemIds.java
@@ -1,15 +1,8 @@
 package paxton.catears;
 
-import com.geckolib.animatable.GeoItem;
-import com.geckolib.animatable.instance.AnimatableInstanceCache;
-import com.geckolib.animatable.manager.AnimatableManager;
-import com.geckolib.util.GeckoLibUtil;
-
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.equipment.ArmorMaterial;
-import net.minecraft.world.item.equipment.ArmorType;
 import net.minecraft.resources.Identifier;
 
 public class ItemIds {
@@ -22,21 +15,7 @@ public class ItemIds {
 
 	public static final ResourceKey<Item> ORB = create("orb");
 
-	public class ExampleArmorItem extends Item implements GeoItem {
-		private final AnimatableInstanceCache geoCache = GeckoLibUtil.createInstanceCache(this);
+	public static final ResourceKey<Item> DARK_ORB = create("dark_orb");
 
-    	public ExampleArmorItem(ArmorMaterial material, ArmorType type, Properties properties) {
-        	super(properties.humanoidArmor(material, type));
-    	}
-
-		@Override
-    	public void registerControllers(final AnimatableManager.ControllerRegistrar controllers) {
-        	// We can fill this in later
-    	}
-
-    	@Override
-    	public AnimatableInstanceCache getAnimatableInstanceCache() {
-        	return this.geoCache;
-    	}
-	}
+	public static final ResourceKey<Item> FOP_ORB = create("fop_orb");
 }
--- src/main/java/paxton/catears/Orb.java
+++ src/main/java/paxton/catears/Orb.java
@@ -6,11 +6,13 @@ import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.component.Consumable;
 import net.minecraft.world.item.component.Consumables;
 import net.minecraft.world.item.consume_effects.ApplyStatusEffectsConsumeEffect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Orb {
     public static final Consumable ORB_CONSUMABLE_COMPONENT = Consumables.defaultFood()
 		// The duration is in ticks, 20 ticks = 1 second
-		.onConsume(new ApplyStatusEffectsConsumeEffect(new MobEffectInstance(MobEffects.POISON, 6 * 20, 1), 1.0f))
+		.onConsume(CatEars.LOGGER.info("eated a orb :3"))
 		.build();
     public static final FoodProperties ORB_COMPONENT = new FoodProperties.Builder()
 		.alwaysEdible()
